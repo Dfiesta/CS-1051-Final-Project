61c2526371ad525748a8e763a390aaa6b2ecd3a7
Dfiesta <81384151+Dfiesta@users.noreply.github.com>
2021-04-26 22:29:05 -0400

    Update Sorry Board Game Project.py

--- Sorry Board Game Project.py
+++ Sorry Board Game Project.py
@@ -1,8 +1,83 @@
 import random
+import turtle
+import tkinter
+
+turtle.setup(850, 850)
+
+window = turtle.Screen()
+window.title("Solitary Sorry!")
+
+canvas = window.getcanvas()
+turtle.setworldcoordinates(-425, -425, 425, 425)
+
+game_board_img = tkinter.PhotoImage(file="Sorry!-Board-Background.gif")
+canvas.create_image(0, 0, anchor="center", image=game_board_img)
+
+window.register_shape("Blue_pawn.gif")
+blue_pawn_1 = turtle.Turtle()
+blue_pawn_1.shape("Blue_pawn.gif")
+blue_pawn_1.penup()
+blue_pawn_1.goto(175, -325)
+
+blue_pawn_2 = turtle.Turtle()
+blue_pawn_2.shape("Blue_pawn.gif")
+blue_pawn_2.penup()
+blue_pawn_2.goto(175, -325)
+
+blue_pawn_3 = turtle.Turtle()
+blue_pawn_3.shape("Blue_pawn.gif")
+blue_pawn_3.penup()
+blue_pawn_3.goto(175, -325)
+
+window.register_shape("Yellow_pawn.gif")
+yellow_pawn_1 = turtle.Turtle()
+yellow_pawn_1.shape("Yellow_pawn.gif")
+yellow_pawn_1.penup()
+yellow_pawn_1.goto(-325, -175)
+
+yellow_pawn_2 = turtle.Turtle()
+yellow_pawn_2.shape("Yellow_pawn.gif")
+yellow_pawn_2.penup()
+yellow_pawn_2.goto(-325, -175)
+
+yellow_pawn_3 = turtle.Turtle()
+yellow_pawn_3.shape("Yellow_pawn.gif")
+yellow_pawn_3.penup()
+yellow_pawn_3.goto(-325, -175)
+
+window.register_shape("Green_pawn.gif")
+green_pawn_1 = turtle.Turtle()
+green_pawn_1.shape("Green_pawn.gif")
+green_pawn_1.penup()
+green_pawn_1.goto(-175, 325)
+
+green_pawn_2 = turtle.Turtle()
+green_pawn_2.shape("Green_pawn.gif")
+green_pawn_2.penup()
+green_pawn_2.goto(-175, 325)
+
+green_pawn_3 = turtle.Turtle()
+green_pawn_3.shape("Green_pawn.gif")
+green_pawn_3.penup()
+green_pawn_3.goto(-175, 325)
+
+window.register_shape("Red_pawn.gif")
+red_pawn_1 = turtle.Turtle()
+red_pawn_1.shape("Red_pawn.gif")
+red_pawn_1.penup()
+red_pawn_1.goto(325, 175)
+
+red_pawn_2 = turtle.Turtle()
+red_pawn_2.shape("Red_pawn.gif")
+red_pawn_2.penup()
+red_pawn_2.goto(325, 175)
+
+red_pawn_3 = turtle.Turtle()
+red_pawn_3.shape("Red_pawn.gif")
+red_pawn_3.penup()
+red_pawn_3.goto(325, 175)
 
 # players = ["Player 1", "Player 2", "Player 3", "Player 4"]
-#
-# colors = ["Blue", "Yellow", "Green", "Red"]
 #
 # blue_pawns = ["Blue 1", "Blue 2", "Blue 3"]
 # yellow_pawns = ["Yellow 1", "Yellow 2", "Yellow 3"]
@@ -12,15 +87,12 @@ import random
 # card_dict = {"1": 1, "2": 2, "3": 3, "4": -4, "5": 5, "7": seven(), "8": 8, "10": ten(), "11": eleven(), "12": 12,
 #              "Sorry!": sorry()}
 #
-#
-# # need to define the space each pawn for each player is on
-# pawn_space = 0
 
-deck_cards = ["1", "1", "1", "1", "2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5", "7",
-              "7", "7", "7", "8", "8", "8", "8", "10", "10", "10", "10", "11", "11", "11", "11", "12", "12", "12", "12",
-              "Sorry!", "Sorry!", "Sorry!", "Sorry!"]
+colors = ["Blue", "Yellow", "Green", "Red"]
 
-discard_pile = []
+DECK_CARDS = ["1", "1", "1", "1", "1", "2", "2", "2", "2", "3", "3", "3", "3", "4", "4", "4", "4", "5", "5", "5", "5",
+              "7", "7", "7", "7", "8", "8", "8", "8", "10", "10", "10", "10", "11", "11", "11", "11", "12", "12", "12",
+              "12", "Sorry!", "Sorry!", "Sorry!", "Sorry!"]
 
 
 class Player:
@@ -42,35 +114,93 @@ class Pieces:
     def __init__(self, color, number):
         self.color = color  # blue, yellow, green, or red
         self.number = number  # pawn number
-        self.start = None
-        self.safety = None
-        self.home = None
+        self.start = True
+        self.safety = False
+        self.home = False
+
+    # Coordinates of starts
+        if self.color == "Blue":
+            self.coordinates = [175, -325]
+        elif self.color == "Yellow":
+            self.coordinates = [-325, -175]
+        elif self.color == "Green":
+            self.coordinates = [-175, 325]
+        elif self.color == "Red":
+            self.coordinates = [325, 175]
 
     def can_swap(self, other):
         # if the player piece and other piece is not in start, the safety zone, or home, can swap
-        if self.start is not None and self.safety is not None and self.home is not None:
-            if other.start is not None and other.safety is not None and other.home is not None:
+        if self.start is False and self.safety is False and self.home is False:
+            if other.start is False and other.safety is False and other.home is False:
                 return True
             else:
                 return False
-        else:
-            pass
 
 
 class Deck:
-    def __init__(self, card):
-        self.card = card
+    def __init__(self, deck_cards):
+        self.shuffled_deck = random.shuffle(deck_cards)
+        self.discard_pile = []
 
     def draw_card(self):
-        shuffled_deck = random.shuffle(deck_cards)
-        card_drawn = shuffled_deck[0]
+        if len(self.shuffled_deck) > 0:
+            card_drawn = self.shuffled_deck[0]
+            self.shuffled_deck.remove(card_drawn)
+        else:
+            self.shuffled_deck = self.discard_pile
+            self.discard_pile.clear()
+            card_drawn = self.shuffled_deck[0]
+            self.shuffled_deck.remove(card_drawn)
         return card_drawn
 
     def discard_card(self, card_drawn):
-        deck_cards.remove(card_drawn)
-        discard_pile.append(card_drawn)
+        self.discard_pile.append(card_drawn)
 
-    def reshuffle(self, card):
+    def draw_four(self, card_drawn, piece):
+        # Move piece backwards 4 spaces
+        pass
+
+    def draw_seven(self, card_drawn, piece):
+        # Move one pawn forward 7 spaces or split forward move of 7 spaces between two pawns
+        # seven_decision = input("Would you like to split your move between two of your pawns? Yes/No: ")
+        # if seven_decision.lower() == "yes":
+            # ask the player which pawn they would like to move
+            # ask the player how many spaces they would like the pawn they've chosen to move
+            # (if more than two pawns are out of Start), ask the player the second pawn they'd like to move forward
+            # apply remainder of move to second pawn
+        #     pass
+        # else:
+            # pawn_space += 7
+        pass
+
+    def draw_ten(self, card_drawn, piece):
+        # Move one pawn forward 10 spaces or move one pawn backward 1 space
+        # if pawn_space + 10 < 60: # if moving forward 10 spaces doesn't take the player past "Home"
+        #     ten_decision = input("Would you like to move your pawn forward 10 spaces? Yes/No: ")
+        #     if ten_decision.lower() == "yes":
+                # pawn_space += 10
+            # else:
+                # pawn_space -= 1
+        # else:
+            # pawn_space -= 1
+        pass
+
+    def draw_eleven(self, card_drawn, piece):
+        # Move one pawn forward 11 spaces or switch one pawn with an opponent's
+        # if pawn_space + 11 < 60: # if moving forward 11 spaces doesn't take the player past "Home"
+        #     eleven_decision = input("Would you like to switch pawns with another player? Yes/No: ")
+        #     if eleven_decision.lower() == "yes":
+                # ask player what pawn they would like to switch with
+                # check to make sure pawn is not in safety zone, "Home", or Start
+        #     else:
+        #         pawn_space += 11
+        # else:
+        #     pawn_space += 0
+        pass
+
+    def draw_sorry(self, card_drawn, piece):
+        # Move one pawn from player's start area to take the place of an opponent's pawn, which must return to start
+        # or move one pawn forward 4 spaces
         pass
 
 
@@ -106,74 +236,64 @@ class Board:
         # Red starts at row[5], col[15]
         # Red safety zone is row[2], col[10-15]
         # Red home is row[2], col[10]
-        # [Yellow/Blue/Red] Slide Start row[1], col[2]
-        # [Yellow/Blue/Red] Slide End row[1], col[5]
-        # [Yellow/Blue/Red] Slide Start row[1], col[10]
-        # [Yellow/Blue/Red] Slide End row[1], col[14]
-        # [Green/Yellow/Blue] Slide Start row[2], col[16]
-        # [Green/Yellow/Blue] Slide End row[5], col[16]
-        # [Green/Yellow/Blue] Slide Start row[10], col[16]
-        # [Green/Yellow/Blue] Slide End row[14], col[16]
-        # [Red/Green/Yellow] Slide Start row[16], col[15]
-        # [Red/Green/Yellow] Slide End row[16], col[12]
-        # [Red/Green/Yellow] Slide Start row[16], col[7]
-        # [Red/Green/Yellow] Slide End row[16], col[3]
-        # [Blue/Red/Green] Slide Start row[15], col[1]
-        # [Blue/Red/Green] Slide End row[12], col[1]
-        # [Blue/Red/Green] Slide Start row[7], col[1]
-        # [Blue/Red/Green] Slide End row[3], col[1]
 
-    def place(self, piece, row, col):
-        self.game_board[row][col] = piece
+    # def place(self, piece, row, col):
+    #     self.game_board[row][col] = piece
 
-    def move_piece(self, card_drawn):
+    def move_piece(self, piece, card_drawn):
         # move piece based on card drawn, card function
-        # move piece 50, if that move takes piece off the board rotate 90 and continue
+        # set piece.start = False
+        if piece.start is True:
+            if piece.color == "Blue":
+                # Select pawn to move, and move out of start
+                piece.start = False
+            if piece.color == "Yellow":
+                # Select pawn to move, and move out of start
+                piece.start = False
+            if piece.color == "Green":
+                # Select pawn to move, and move out of start
+                piece.start = False
+            if piece.color == "Red":
+                # Select pawn to move, and move out of start
+                piece.start = False
+        else:
+            # Select pawn to move, and move pawn number of spaces based on card_drawn
+            # If move takes pawn off board, rotate 90 and continue
+            pass
+
+    def slide(self, piece):
+        # Blue slides:
+        # Start: [-375, -325]; End: [-375, -175]
+        # Start: [-375, 75]; End: [-375, 275]
+        # Start: [-325, 375]; End: [-175, 375]
+        # Start: [75, 375]; End: [275, 375]
+        # Start: [375, 325]; End: [375, 175]
+        # Start: [375, -75]; End: [375, -275]
+        # Yellow slides:
+        # Start: [-325, 375]; End: [-175, 375]
+        # Start: [75, 375]; End: [275, 375]
+        # Start: [375, 325]; End: [375, 175]
+        # Start: [375, -75]; End: [375, -275]
+        # Start: [325, -375]; End: [175, -375]
+        # Start: [-75, -375]; End: [-275, -375]
+        # Green slides:
+        # Start: [375, 325]; End: [375, 175]
+        # Start: [375, -75]; End: [375, -275]
+        # Start: [325, -375]; End: [175, -375]
+        # Start: [-75, -375]; End: [-275, -375]
+        # Start: [-375, -325]; End: [-375, -175]
+        # Start: [-375, 75]; End: [-375, 275]
+        # Red slides:
+        # Start: [325, -375]; End: [175, -375]
+        # Start: [-75, -375]; End: [-275, -375]
+        # Start: [-375, -325]; End: [-375, -175]
+        # Start: [-375, 75]; End: [-375, 275]
+        # Start: [-325, 375]; End: [-175, 375]
+        # Start: [75, 375]; End: [275, 375]
         pass
 
 
-# def seven():
-#     # code
-#     seven_decision = input("Would you like to split your move between two of your pawns? Yes/No: ")
-#     if seven_decision.lower() == "yes":
-#         # ask the player which pawn they would like to move
-#         # ask the player how many spaces they would like the pawn they've chosen to move
-#         # (if more than two pawns are out of Start), ask the player the second pawn they'd like to move forward
-#         # apply remainder of move to second pawn
-#         pass
-#     else:
-#         pawn_space += 7
-#     pass
-#
-#
-# def ten():
-#     # code
-#     if pawn_space + 10 < 60: # if moving forward 10 spaces doesn't take the player past "Home"
-#         ten_decision = input("Would you like to move your pawn forward 10 spaces? Yes/No: ")
-#         if ten_decision.lower() == "yes":
-#             pawn_space += 10
-#         else:
-#             pawn_space -= 1
-#     else:
-#         pawn_space -= 1
-#     pass
-#
-#
-# def eleven():
-#     # code
-#     if pawn_space + 11 < 60: # if moving forward 11 spaces doesn't take the player past "Home"
-#         eleven_decision = input("Would you like to switch pawns with another player? Yes/No: ")
-#         if eleven_decision.lower() == "yes":
-#             # ask player what pawn they would like to switch with
-#             # check to make sure pawn is not in safety zone, "Home", or Start
-#         else:
-#             pawn_space += 11
-#     else:
-#         pawn_space += 0
-#     pass
-#
-#
-# def sorry():
-#     # code
-#     pass
-#
+turtle.exitonclick()
+
+green_1 = Pieces("Green", 1)
+green_1.start = False
